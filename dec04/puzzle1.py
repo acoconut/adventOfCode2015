7f8d5b33de56db94806b2f044882b4b02f26bc90
Ana Cutillas <acutillas@stratio.com>
2018-11-20 12:30:17 +0100

    Solved day 4

---
+++ dec04/puzzle1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Advent of code
+# Day 4 - Part 1
+
+from hashlib import md5
+
+# Input
+my_input = 'bgvyzdsv'
+
+cont = 0 
+not_found = True
+while not_found:
+    md5_hash = md5((my_input+str(cont)).encode('utf-8')).hexdigest()
+    if md5_hash.startswith('00000'):
+        break
+    cont = cont + 1
+
+
+print (cont)
